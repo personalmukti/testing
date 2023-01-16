854d23ecf4c46ed9635a7b26045cca67fb4c9770
Mukti AD <mukti.devmail@gmail.com>
2023-01-16 09:48:35 +0700

    Menambahkan method show, update dan destroy

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Resources\PostResource;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -47,5 +48,65 @@ class PostController extends Controller
 
         // return respon hasil
         return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
+    }
+
+    public function show(Post $post)
+    {
+        //return single post as a resource
+        return new PostResource(true, 'Data Post Ditemukan!', $post);
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //check if image is not empty
+        if ($request->hasFile('image')) {
+
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts/' . $post->image);
+
+            //update post with new image
+            $post->update([
+                'image'     => $image->hashName(),
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+        } else {
+
+            //update post without image
+            $post->update([
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+        }
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
+    }
+
+    public function destroy(Post $post)
+    {
+        //delete image
+        Storage::delete('public/posts/' . $post->image);
+
+        //delete post
+        $post->delete();
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Dihapus!', null);
     }
 }
