0f727c54cd5f4fff6ca136bb7313224ab99ba046
Mukti AD <mukti.devmail@gmail.com>
2023-01-16 09:46:57 +0700

    Menambahkan method store

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -17,5 +18,34 @@ class PostController extends Controller
 
         // mengambil collection sebagai resources
         return new PostResource(true, 'List Data Posts', $posts);
+    }
+
+    public function store(Request $request)
+    {
+        // definiskan rule untuk validator
+        $validator = Validator::make($request->all(), [
+            'image'     => 'required|image|mimes:png,jpg, jpeg, gif, svg|max:2048',
+            'title'     => 'required',
+            'content'   => 'required'
+        ]);
+
+        // Cek dulu jika validasi gagal
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // upload gambar
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        // setelah upload image buat post untuk data
+        $post = Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content,
+        ]);
+
+        // return respon hasil
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
     }
 }
